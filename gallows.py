afdf19873c3d1564cf4d4c782868fcd85d2990b1
jacksondan <jackson.daniel.almeida@gmail.com>
2021-07-21 19:12:47 -0300

    Implementing Gallows code

--- gallows.py
+++ gallows.py
@@ -4,25 +4,43 @@ def play():
     print("****** Gallows game *******")
     print("***************************")
 
-    key_word = "banana"
+    key_word = "banana".strip().upper()
     quantity = len(key_word)
-    print(quantity)
+    letter_list = []
     hanged = False
     correct_word = False
+    chances = 10
+
+    for i in range(0,quantity):
+        letter_list.append("_")
 
     while(not hanged and not correct_word):
+        print()
+        print("**You have {} chances".format(chances))
+        print("Secret Word")
+        print(letter_list)
+
         kick = input("Type one word: ")
-        kick = kick.strip()
+        kick = kick.strip().upper()
         found = False
         i = 0
-
         for letter in key_word:
-            if(kick.lower() == letter.lower()):
+            if(kick == letter):
                 found = True
-                print("*** Letter {} was found on index {}".format(letter,i) )
+                letter_list[i] = letter
             i += 1
         if(not found):
             print("Letter doesn't found")
+        if(not "_" in letter_list):
+            correct_word = True
+            print(letter_list)
+            print("You got all the letters right !!!")
+            print("Congratulations !!!!!!!!")
+
+        chances -= 1
+        if(chances == 0 and correct_word == False):
+            hanged = True
+            print("You hanged yourself")
 
 if(__name__ == "__main__"):
     play()
