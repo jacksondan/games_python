b7c1ae4e44ff5faf636f731d11d263cfda38fae9
jacksondan <jackson.daniel.almeida@gmail.com>
2021-07-21 12:07:20 -0300

    It's necessary to commit and push the changes to see the conflicts

--- gallows.py
+++ gallows.py
@@ -5,13 +5,24 @@ def play():
     print("***************************")
 
     key_word = "banana"
+    quantity = len(key_word)
+    print(quantity)
     hanged = False
     correct_word = False
 
     while(not hanged and not correct_word):
         kick = input("Type one word: ")
-        for word in key_word:
-            print(word)
+        kick = kick.strip()
+        found = False
+        i = 0
+
+        for letter in key_word:
+            if(kick.lower() == letter.lower()):
+                found = True
+                print("*** Letter {} was found on index {}".format(letter,i) )
+            i += 1
+        if(not found):
+            print("Letter doesn't found")
 
 if(__name__ == "__main__"):
     play()
--- games.py
+++ games.py
@@ -1,14 +1,15 @@
 import divination
+import gallows
 print("************************")
 print("******* Welcome ********")
 print("*** Choose your game ***")
 print("************************")
 
-choice = int(input("(1) Divination, (2) Game Two: "))
+choice = int(input("(1) Divination, (2) Gallows: "))
 
 if(choice == 1):
     divination.play()
 elif(choice == 2):
-    print("Game 02")
+    gallows.play()
 else:
     print("Choice invalid")
