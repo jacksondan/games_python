5af90c2c3b05ed3bbe5d1123605c23ff2c080aae
jacksondan <jackson.daniel.almeida@gmail.com>
2021-07-21 06:19:57 -0300

    Commiting informations to test conflicts

---
+++ gallows.py
@@ -0,0 +1,9 @@
+def play():
+    print("***************************")
+    print("****** gallows game ******")
+    print("***************************")
+
+    key_word = "banana"
+
+if(__name__ == "__main__"):
+    play()
